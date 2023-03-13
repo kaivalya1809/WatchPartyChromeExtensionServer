44361b10f7af75f1099437545e1140311af49925
kaivalya1809 <kaivalya.ingle1809@gmail.com>
2023-03-13 15:17:04 +0530

    ahmm 3000 to port lol

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const WebSocket = require("ws");
-
+const port = 3000;
 const wss  = new WebSocket.Server({server:server});
 wss.on('connection', function connection(ws) {
     console.log('A new client Connected!');
@@ -17,4 +17,4 @@ wss.on('connection', function connection(ws) {
   });
 app.get('/',(req,res)=>res.send("Hello World"));
 
-server.listen(3000,()=>console.log("Listening on port"));
+server.listen(port,()=>console.log("Listening on port"+port));
